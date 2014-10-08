02913f70bc3da1012d51282de68fdc0efd04eb05
fono09 <fono@fono.jp>
2014-10-08 23:45:43 +0900

    Complete MergeSort in C

--- MergeSort.c
+++ MergeSort.c
@@ -6,94 +6,108 @@ void merge(int *a,int p, int q, int r){
 
 	int n_1 = q - p;
 	int n_2 = r - q;
+	//探索範囲確保
+
+	int i,j,k;
+	//ループ用変数3つ宣言
 
 	int *L,*R;
+	//ポインタを宣言
 
 	L = (int*)malloc(sizeof(int)*n_1+1);
 	R = (int*)malloc(sizeof(int)*n_2+1);
+	//必要分のみ確保
 
-	int i,j,k;
-	printf("make L[%d] array.....\n",n_1);
 	for(i = 0; i < n_1; i++){
-		printf(" %d |",a[p+i]);
 		L[i] = a[p+i];
 	}
-	printf("\n");
+	//分割左配列生成
 
-	printf("make R[%d] array.....\n",n_2);
 	for(j = 0; j < n_2; j++){
-		printf(" %d |",a[q+j]);
 		R[j] = a[q+j];
 	}
-	printf("\n");
+	//分割右配列生成
 
 	i = 0;
 	j = 0;
+	//L,Rへのコピー済みインデックス
 
 	for(k = p; k < r; k++){
+		//ソート範囲に限り
+
 		if(L[i] <= R[j] && i < n_1 && j < n_2){
+			//インデックスがオーバーランしていなければ比較しより小さい方先にコピー
 			a[k] = L[i];
 			i++;
+
 		}else if(L[i] > R[j] && i < n_1 && j < n_2){
+			//同じく小さい方を先にコピー
 			a[k] = R[j];
 			j++;
 		}else if(i < n_1){
+			//インデックスがオーバランしていない残りをコピー
 			a[k] = L[i];
 			i++;
 		}else{
+			//インデックスがオーバランしていない残りをコピー
 			a[k] = R[j];
 			j++;
 		}
 	}
 
-	printf("Sorted a[%d] to a[%d].....\n",p,r-1);
-	for(k = p; k < r; k++){
-		printf(" %d |",a[k]);
-	}
-	printf("\n");
-
 	free((void *)L);
 	free((void *)R);
+	//確保した領域解放
 
 }
 
 void merge_sort(int *a, int p, int r){
 
-	printf("Called merge_sort(*a,%d,%d)\n",p,r);
 	if(p+1 < r){
+		//ループ条件を満たす間
 		int q=(p+r)/2;
 		merge_sort(a,p,q);
 		merge_sort(a,q,r);
+		//二分探索し
 		merge(a,p,q,r);
+		//マージ
 	}
 }
 int main(){
 
 	
-	int i;
+	int i,j;
 	srand((unsigned)time(NULL));
+	//乱数初期化
 
-	int num_of_data = rand()%50 + 50;
+	printf("最低データ数を入力してください:");
+	scanf("%d",&j);
+	//最低データ数を入力
+
+	int num_of_data = rand()%j + j;
+	//データ数を最低10000データでランダム
 	int data[num_of_data];
+	//データ数分確保
 
 	for(i = 0; i < num_of_data; i++){
 
-		data[i] = rand()%100;
+		data[i] = rand()%j;
 		printf(" %d |",data[i]);
 
 	}
 	printf("\n");
-	printf("Complete Initialize.....\n");
+	//データをランダムで埋めつつ初期出力
 
 	merge_sort(data,0,num_of_data);
+	//先頭ポインタを与え、データ数を指定するのみでマージソート
 
-	printf("Complete Sort.....\n");
 	for(i = 0; i < num_of_data; i++){
 
 		printf(" %d |",data[i]);
 
 	}
 	printf("\n");
+	//ソート確認出力
 	
 	return 0;
 }
