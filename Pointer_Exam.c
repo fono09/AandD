c10e872af25e02906dd864ae385195457a09eb28
fono09 <fono@fono.jp>
2014-12-06 23:30:50 +0900

    ポインタの配列のポインタの配列へのポインタである

--- Pointer_Exam.c
+++ Pointer_Exam.c
@@ -1,14 +1,13 @@
 #include<stdio.h>
 #include<time.h>
 
-#define LENGTH 10
+#define LENGTH 2
 
-int ref(int *q){
+int ref(void *q){
 
 	int i;
 	for(i = 0; i < LENGTH; i++){
 
-		*(q+i) = *(q+i)*2;
 		printf("q = %p,i = %d,q+i = %p\n",q,i,q+i);
 
 	}
@@ -18,19 +17,26 @@ int ref(int *q){
 
 int main(){
 
-	int *p;
-	int test[LENGTH];
-	int **test_table[LENGTH][LENGTH];
-
+	int (*p)[LENGTH][LENGTH];
+	int test[LENGTH][LENGTH][LENGTH];
 
 	srand((unsigned int)time(NULL));
 
 	p = test;
-	int i;
+	int i,j,k;
 	for(i = 0; i < LENGTH; i++){
 
-		p[i] = rand()%LENGTH;
-		printf("test[i] = %d,p[i] = %d,i[p] = %d,*(p+i) = %d\n",test[i],p[i],i[p],*(p+i));
+		for(j = 0; j < LENGTH; j++){
+
+			for(k = 0; k < LENGTH; k++){
+
+
+				p[i][j][k] = rand()%LENGTH;
+				printf("test[i][j][k] = %d,p[i][j][k] = %d\n",test[i][j][k],p[i][j][k]);
+
+			}
+
+		}
 
 	}
 	printf("test = %p\np = %p\n",test,p);
@@ -41,7 +47,15 @@ int main(){
 
 	for(i = 0; i < LENGTH; i++){
 
-		printf("*(p+i) = %d\n",*(p+i));
+		for(j = 0; j < LENGTH; j++){
+
+			for(k = 0; k < LENGTH; k++){
+
+				printf("p[i][j][k] = %d\n",p[i][j][k]);
+
+			}
+
+		}
 
 	}
 
